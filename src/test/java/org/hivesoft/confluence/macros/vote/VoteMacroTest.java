9f965ab2ccd9457b8e671f074fbb9699e14a02ea
drohne1673 <drohne1673@gmx.net>
2013-10-26 16:06:26 -0700

    first simple test for execution of a macro / mandatory parameter

--- src/test/java/org/hivesoft/confluence/macros/vote/VoteMacroTest.java
+++ src/test/java/org/hivesoft/confluence/macros/vote/VoteMacroTest.java
@@ -1,29 +1,39 @@
 package org.hivesoft.confluence.macros.vote;
 
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.core.ContentPropertyManager;
 import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
 import com.atlassian.confluence.pages.Page;
 import com.atlassian.confluence.pages.PageManager;
+import com.atlassian.confluence.renderer.PageContext;
 import com.atlassian.confluence.spaces.SpaceManager;
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
 import com.atlassian.confluence.user.UserAccessor;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
+import com.atlassian.user.impl.DefaultUser;
+import org.hivesoft.confluence.admin.callbacks.SurveyPluginSettings;
 import org.hivesoft.confluence.macros.utils.SurveyUtilsTest;
 import org.hivesoft.confluence.macros.vote.model.Ballot;
 import org.hivesoft.confluence.macros.vote.model.Choice;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class VoteMacroTest {
+    private final static DefaultUser SOME_USER1 = new DefaultUser("someUser1", "someUser1 FullName", "some1@testmail.de");
 
     PageManager mockPageManager = mock(PageManager.class);
     SpaceManager mockSpaceManager = mock(SpaceManager.class);
@@ -33,6 +43,8 @@ public class VoteMacroTest {
     TemplateRenderer mockTemplateRenderer = mock(TemplateRenderer.class);
     XhtmlContent mockXhtmlContent = mock(XhtmlContent.class);
     PluginSettingsFactory mockPluginSettingsFactory = mock(PluginSettingsFactory.class);
+
+    ConversionContext mockConversionContext = mock(ConversionContext.class);
 
     HttpServletRequest mockRequest = mock(HttpServletRequest.class);
 
@@ -44,6 +56,39 @@ public class VoteMacroTest {
 
         when(mockRequest.getParameter(VoteMacro.REQUEST_PARAMETER_BALLOT)).thenReturn(SurveyUtilsTest.SOME_BALLOT);
         when(mockRequest.getParameter(VoteMacro.REQUEST_PARAMETER_CHOICE)).thenReturn(SurveyUtilsTest.SOME_CHOICE);
+        AuthenticatedUserThreadLocal.setUser(SOME_USER1);
+    }
+
+    @After
+    public void tearDown() {
+        AuthenticatedUserThreadLocal.setUser(null);
+
+    }
+
+    @Test(expected = MacroExecutionException.class)
+    public void test_execute_noTitleParameter_exception() throws Exception {
+        classUnderTest.execute(new HashMap(), "", mockConversionContext);
+    }
+
+    /**
+     * It is unclear how to fake velocity stuff, so long this test will be ignored!
+     */
+    @Ignore
+    @Test
+    public void test_execute_simpleMacroWithTitle_exception() throws Exception {
+        final HashMap<String, String> parameters = new HashMap<String, String>();
+        parameters.put(VoteMacro.KEY_TITLE, "someTitle");
+
+        ContentEntityObject somePage = new Page();
+        somePage.setBodyAsString("{vote:title=someTitle}{vote}");
+        final PageContext pageContext = new PageContext(somePage);
+
+        when(mockConversionContext.getEntity()).thenReturn(somePage);
+        when(mockConversionContext.getPageContext()).thenReturn(pageContext);
+        when(mockPluginSettingsFactory.createGlobalSettings()).thenReturn(new SurveyPluginSettings());
+
+        final String macroResultAsString = classUnderTest.execute(parameters, "", mockConversionContext);
+        //assertTrue(macroResultAsString.contains("someTitle"));
     }
 
     @Test
