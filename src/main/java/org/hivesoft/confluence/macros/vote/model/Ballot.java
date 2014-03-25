c82aaf6b0ec9c57de7bf65d20e2af02a0fcb287e
drohne1673 <drohne1673@gmx.net>
2014-03-25 23:47:43 +0100

    #17: fixed some glitches with survey locking. Added subtext within title column. Added current number above choiceName

--- src/main/java/org/hivesoft/confluence/macros/vote/model/Ballot.java
+++ src/main/java/org/hivesoft/confluence/macros/vote/model/Ballot.java
@@ -168,6 +168,14 @@ public class Ballot {
     return config.getStartBound() > getEndBound() ? config.getStartBound() : getEndBound();
   }
 
+  public int getCurrentValueByIndex(int index) {
+    if (config.getIterateStep() > 0) {
+      return config.getStartBound() + config.getIterateStep() * index;
+    } else {
+      return config.getStartBound() - config.getIterateStep() * index;
+    }
+  }
+
   public int getAveragePercentage(float average) {
     if (config.getIterateStep() < 0) {
       return (int) (-1 * ((float) getLowerBound() - average - (float) config.getIterateStep()) * 100.0f / ((float) getUpperBound() - (float) getLowerBound() - (float) config.getIterateStep()));
@@ -185,6 +193,9 @@ public class Ballot {
       weightedChoices += choiceIndex * voteCount;
       choiceIndex--;
       totalVoteCount += voteCount;
+    }
+    if (totalVoteCount == 0) {
+      return 0;
     }
     return weightedChoices * 100 / ((choices.values().size() - 1) * totalVoteCount);
   }
