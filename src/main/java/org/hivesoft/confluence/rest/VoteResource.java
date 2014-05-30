c61d2a25430381dcd21731f937447e1b6883cf66
drohne1673 <drohne1673@gmx.net>
2014-05-30 07:50:20 +0200

    some experimental code for reconstructing

--- src/main/java/org/hivesoft/confluence/rest/VoteResource.java
+++ src/main/java/org/hivesoft/confluence/rest/VoteResource.java
@@ -10,16 +10,24 @@
  */
 package org.hivesoft.confluence.rest;
 
+import com.atlassian.confluence.content.render.xhtml.DefaultConversionContext;
+import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.pages.PageManager;
+import com.atlassian.confluence.xhtml.api.MacroDefinition;
+import com.atlassian.confluence.xhtml.api.MacroDefinitionHandler;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 import com.atlassian.extras.common.log.Logger;
 import com.atlassian.sal.api.message.I18nResolver;
 import com.atlassian.sal.api.transaction.TransactionTemplate;
 import org.hivesoft.confluence.macros.enums.VoteAction;
+import org.hivesoft.confluence.macros.survey.SurveyMacro;
+import org.hivesoft.confluence.macros.survey.model.Survey;
 import org.hivesoft.confluence.macros.utils.SurveyManager;
-import org.hivesoft.confluence.macros.vote.VoteConfig;
+import org.hivesoft.confluence.macros.utils.SurveyUtils;
+import org.hivesoft.confluence.macros.vote.VoteMacro;
 import org.hivesoft.confluence.macros.vote.model.Ballot;
+import org.hivesoft.confluence.rest.exceptions.MacroReconstructionException;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -29,6 +37,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Path("/pages/{pageId}/votes")
 public class VoteResource {
@@ -69,5 +80,37 @@ public class VoteResource {
     //surveyManager.recordVote(ballot, contentEntityObject, choiceName, voteAction);
 
     return Response.ok().build();
+  }
+
+  private List<Ballot> reconstructBallotByTitleFromSurveyOrVote(final String ballotTitle, final ContentEntityObject contentEntityObject) throws MacroReconstructionException {
+    final List<Ballot> ballotsFound = new ArrayList<Ballot>();
+    try {
+      xhtmlContent.handleMacroDefinitions(contentEntityObject.getBodyAsString(), new DefaultConversionContext(contentEntityObject.toPageContext()), new MacroDefinitionHandler() {
+                @Override
+                public void handle(MacroDefinition macroDefinition) {
+                  final Map<String, String> parameters = macroDefinition.getParameters();
+                  String currentTitle = SurveyUtils.getTitleInMacroParameters(parameters);
+                  if (SurveyMacro.SURVEY_MACRO.equals(macroDefinition.getName())) {
+                    //TODO: on a survey check the sub-items (ballots)
+
+                    final Survey survey = surveyManager.reconstructSurveyFromPlainTextMacroBody(macroDefinition.getBodyText(), contentEntityObject, macroDefinition.getParameters());
+                    //survey.setTitle(surveyTitle);
+                    //<ballotsFound.add(survey);
+                  } else if (VoteMacro.VOTE_MACRO.equals(macroDefinition.getName())) {
+                    //TODO reconstruct the ballot "directly"
+                  }
+                }
+              }
+      );
+    } catch (XhtmlException e) {
+      final String message = "There was a problem while parsing the Xhtml content: " + e.getMessage() + " for ballotTitle: " + ballotTitle;
+      LOG.error(message, e);
+      throw new MacroReconstructionException(message, e);
+    }
+
+    if (ballotsFound.isEmpty()) {
+      throw new MacroReconstructionException("Could not find the specified survey macro with title " + ballotTitle + " on the specified page!");
+    }
+    return ballotsFound;
   }
 }
