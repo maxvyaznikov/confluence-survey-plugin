373995bad02630bb9e6a5f924386c20d793402a8
drohne1673 <drohne1673@gmx.net>
2015-03-01 23:14:22 +0100

    yearly adjustment of the year :D

--- src/main/java/org/hivesoft/confluence/macros/vote/VoteConfig.java
+++ src/main/java/org/hivesoft/confluence/macros/vote/VoteConfig.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2006-2014, Confluence Community
+ * Copyright (c) 2006-2015, Confluence Community
  * All rights reserved.
  * <p/>
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
